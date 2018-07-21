9d32d92c2d140674f731a35299668f9dee469e86
ikeoha chidi <ikeohachidi@gmail.com>
2018-07-21 15:59:20 +0100

    added style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 import Main from "./components/Main";
 import Nav from "./components/Nav";
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = () => {
-    return(
-        <div>
-            <NavLink to="/weather" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink>
-            <NavLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
-            <NavLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
-        </div>
-    )
+class Nav extends React.Component {
+    constructor() {
+        super()
+        this.onSearch = this.onSearch.bind(this)
+    }
+    onSearch(e) {
+        e.preventDefault()
+        alert("not wired")
+    }
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+    
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <NavLink to="/weather" className="nav-link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink to="/about" className="nav-link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink to="/examples" className="nav-link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0" onSubmit={this.onSearch}>
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search Weather" aria-label="Search"/>
+                        <button class="btn btn-primary" type="submit">Get Weather</button>
+                    </form>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Nav;
+
+        // let old = (
+//     <div>
+//         <NavLink to="/weather" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink>
+//         <NavLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
+//         <NavLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
+//     </div>
+// )
+

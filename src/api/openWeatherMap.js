22dfd6aed3a151125b4e71175aaa1af6890e7b39
ikeoha chidi <ikeohachidi@gmail.com>
2018-07-21 04:47:59 +0100

    change api url

--- src/api/openWeatherMap.js
+++ src/api/openWeatherMap.js
@@ -1,6 +1,6 @@
 const axios = require("axios")
 
-const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?appid=c7f42dff99a576f1eab8a55f0f7f1411&units=metric"
+const OPEN_WEATHER_MAP_URL = "https://api.openweathermap.org/data/2.5/weather?appid=c7f42dff99a576f1eab8a55f0f7f1411&units=metric"
 
 module.exports = {
     getTemp: function(location) {
